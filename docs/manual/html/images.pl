e8a3839f2b085bf5deec9744f0e457f28b7fdaea
Jay Convertino <electrobs@gmail.com>
2024-10-27 13:40:10 -0400

    first version of all docs that render correctly, now its time to actual write the latex document for this repo

--- docs/manual/html/images.pl
+++ docs/manual/html/images.pl
@@ -2,10 +2,10 @@
 # Associate images original text with physical files.
 
 
-$key = q/includegraphics[width=textwidth,height=textheight,keepaspectratio]{srcslashimgslashSPARKLETRON.png};AAT/;
+$key = q/includegraphics[width=0.90textwidth,height=textheight,keepaspectratio]{srcslashimgslashSPARKLETRON.png};AAT/;
 $cached_env_img{$key} = q|<IMG
- STYLE="height: 6.94ex; vertical-align: -0.12ex; " SRC="|."$dir".q|img1.svg"
- ALT="\includegraphics[width=\textwidth,height=\textheight,keepaspectratio]{src/img/SPARKLETRON.png}">|; 
+ STYLE="height: 6.30ex; vertical-align: -0.12ex; " SRC="|."$dir".q|img1.svg"
+ ALT="\includegraphics[width=0.90\textwidth,height=\textheight,keepaspectratio]{src/img/SPARKLETRON.png}">|; 
 
 1;
 

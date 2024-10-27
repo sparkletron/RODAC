094cd3abd84c30206402d562d203e461a5835661
Jay Convertino <electrobs@gmail.com>
2024-10-27 12:24:12 -0400

    cleanup of html page for github.io

---
+++ docs/manual/html/images.pl
@@ -0,0 +1,11 @@
+# LaTeX2HTML 2021.2 (Released July 1, 2021)
+# Associate images original text with physical files.
+
+
+$key = q/includegraphics[width=textwidth,height=textheight,keepaspectratio]{srcslashimgslashSPARKLETRON.png};AAT/;
+$cached_env_img{$key} = q|<IMG
+ STYLE="height: 6.94ex; vertical-align: -0.12ex; " SRC="|."$dir".q|img1.svg"
+ ALT="\includegraphics[width=\textwidth,height=\textheight,keepaspectratio]{src/img/SPARKLETRON.png}">|; 
+
+1;
+
